2912da46d0613618c84ecef6a7ecc63bc785e5ca
su-hwan <xcoxblue@gmail.com>
2023-11-05 17:39:45 +0900

    Add Spring Boot skeleton

---
+++ src/main/java/com/su/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.su.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
